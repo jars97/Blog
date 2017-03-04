e4ff500082cce629249cad973634f50eaabea82c
Raúl Noa <rnoap13@gmail.com>
2017-03-04 02:30:20 -0400

    Primera parte del artículo terminada.

--- Manejo de un documento Excel con AngularJS/app.js
+++ Manejo de un documento Excel con AngularJS/app.js
@@ -4,7 +4,7 @@
 (function(){
     'use strict';
 
-    var app = angular.module('AngularExcel', ['ng', 'angular-js-xlsx', 'ngMdIcons'])
+    var app = angular.module('AngularExcel', ['ng', 'angular-js-xlsx', 'ngMdIcons', 'contenteditable'])
 
         
     .controller('AppCtrl', function ($q, $scope) {
@@ -14,14 +14,19 @@
         var first_sheet_name;
         var jsonData;
         var totalProducts;
-        var workBookAutofill;
+        var newWorkBook;
+        $scope.saveButton = true;
         
 
         /**
-         * Read excel file
+         * Leer documento Excel
          */
 
         $scope.read = function (workbook) {
+
+            $scope.saveButton = false;
+
+            newWorkBook = workbook;
 
             first_sheet_name = workbook.SheetNames[0];
             jsonData = XLSX.utils.sheet_to_json(workbook.Sheets[first_sheet_name]);
@@ -31,20 +36,19 @@
 
             for (var i = 1; i<jsonData.length;i++)
             {
-                for(var name in jsonData[i]) {
-                 //   console.log(name);
-                    var value = jsonData[i][name];
-                   // console.log(value);
-                }
                 $scope.Elements.push(
                     jsonData[i]
                 );
                 $scope.$apply();
-
             }
 
+        };
 
-
+        /**
+         * Salvar documento excel con los cambios realizados
+         */
+        $scope.saveData = function (){
+          alert("This functionality do not work yet");
         };
 
         var drop = document.getElementById('drop');
@@ -58,19 +62,26 @@
             {
                 var reader = new FileReader();
                 name = f.name;
+
                 reader.onload = function(evt) {
                     if(typeof console !== 'undefined')
                         var data = evt.target.result;
 
                     /* if binary string, read with type 'binary' */
                     try {
+                        /*Mostrar el nombre del archivo cargado en el label*/
+                        $scope.name = name;
+                        $scope.saveButton = false;
+                        $scope.$apply();
+
+                        /*Leer el archivo cargado*/
                         workbook2 = XLS.read(data, {type: 'binary'});
                         $scope.read(workbook2);
 
-                        $scope.name = name;
 
                     } catch(e) {
                         console.log("error");
+                        alert("This is not a excel file");
                     }
                 };
                 reader.readAsBinaryString(f);
@@ -90,12 +101,16 @@
         }
 
     });
+
+    /**
+    * Para mostrar el nombre del archivo excel
+    * cargado mediante la opción de búsqueda en el equipo
+    */
     app.directive('fdInput', [function () {
         return {
             link: function ($scope, element, attrs) {
                 element.on('change', function  (evt) {
                     var files = evt.target.files;
-                  //  console.log(files[0].name);
 
                     $scope.name = files[0].name;
 
