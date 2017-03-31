6c2df0332fdb7d9493e772e346f149db4e2ec625
Raúl Noa <rnoap13@gmail.com>
2017-03-31 19:46:46 -0300

    Añadiendo funcionalidad de salvar los cambios realizados a los datos del Excel cargado generando un nuevo docuemnto Excel.

--- Manejo de un documento Excel con AngularJS/app.js
+++ Manejo de un documento Excel con AngularJS/app.js
@@ -7,15 +7,15 @@
     var app = angular.module('AngularExcel', ['ng', 'angular-js-xlsx', 'ngMdIcons', 'contenteditable'])
 
         
-    .controller('AppCtrl', function ($q, $scope) {
+    .controller('AppCtrl', function ($scope) {
         $scope.name = "Select file";
         var name;
-        var workbookNew;
         var first_sheet_name;
         var jsonData;
-        var totalProducts;
         var newWorkBook;
         $scope.saveButton = true;
+        var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
         
 
         /**
@@ -28,19 +28,22 @@
 
             newWorkBook = workbook;
 
-            first_sheet_name = workbook.SheetNames[0];
-            jsonData = XLSX.utils.sheet_to_json(workbook.Sheets[first_sheet_name]);
+            first_sheet_name = newWorkBook.SheetNames[0];
 
-            $scope.Elements = [jsonData[0]];
-            $scope.$apply();
+            $scope.$apply(function(){
 
-            for (var i = 1; i<jsonData.length;i++)
-            {
-                $scope.Elements.push(
-                    jsonData[i]
-                );
-                $scope.$apply();
-            }
+                jsonData = XLSX.utils.sheet_to_json(newWorkBook.Sheets[first_sheet_name]);
+                jsonData[0]["rowIndex"] = "0";
+                $scope.Elements = [jsonData[0]];
+
+                for (var i = 1; i<jsonData.length;i++)
+                {
+                    $scope.Elements.push(
+                        jsonData[i]
+                    );
+                    jsonData[i]["rowIndex"] = i;
+                }
+            });
 
         };
 
@@ -48,8 +51,50 @@
          * Salvar documento excel con los cambios realizados
          */
         $scope.saveData = function (){
-          alert("This functionality do not work yet");
+            //  bookType can be 'xlsx' or 'xlsm' or 'xlsb'
+            var wopts = { bookType:'xlsx', bookSST:false, type:'binary' };
+
+            var wbout = XLSX.write(newWorkBook,wopts);
+
+            function s2ab(s) {
+                var buf = new ArrayBuffer(s.length);
+                var view = new Uint8Array(buf);
+                for (var i=0; i!=s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+                return buf;
+            }
+
+            /**
+             * Capturando la fecha y la hora a la
+             * que se realiza la salva
+             */
+
+            var d = new Date();
+
+            var datestring = d.getDate()  + "-" + (d.getMonth()+1) + "-" + d.getFullYear() + " " +
+                d.getHours() + "-" + d.getMinutes();
+
+            /* the saveAs call downloads a file on the local machine */
+            saveAs(new Blob([s2ab(wbout)],{type:""}), "New Excel file "+datestring+".xlsx");
         };
+
+        $scope.changedCeld = function(val){
+            var numberColumn = event.target.getAttribute('id');
+            var numberArrow = event.target.parentElement.getAttribute('id');
+            var worksheet = newWorkBook.Sheets[first_sheet_name];
+            /**
+             * Definimos la celda con la estructura LETRA + NÚMERO DE FILA
+             * */
+            var address_of_cell = alphabet[numberColumn]+(parseInt(numberArrow)+2);
+
+            /**
+             * Creamos la celda que contiene los datos modificados y
+             * actualizamos los datos
+             * */
+            var newcell = {h: val, r:"<t>"+val+"</t>", t:"s", v:val, w:val};
+            worksheet[address_of_cell] = newcell;
+
+        };
+
 
         var drop = document.getElementById('drop');
         var workbook2;
